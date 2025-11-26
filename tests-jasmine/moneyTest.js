2b0dbdce616085ce4f4a04ecf57957a1911bfa5a
Iam-AshishYadav <20bca1335@cuchd.in>
2025-11-26 10:22:59 +0530

    remove default test and add money tests

--- tests-jasmine/moneyTest.js
+++ tests-jasmine/moneyTest.js
@@ -6,6 +6,15 @@ it('converts cents into dollars', ()=>{
 
 expect(formatCurrency(2095)).toEqual('20.95');
 });
+
+it('works with zero',()=>{
+
+  expect(formatCurrency(0)).toEqual('0.00');
 });
 
-//16.13.48 is where i am 
+it('round up to the neareast cent',()=>{
+  expect(formatCurrency(2000.5)).toEqual('20.01');
+});
+
+});
+
